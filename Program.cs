3de2a37b1196a3bb5bcfa98dd1392e24ee9cd427
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-02-04 07:33:01 +0300

    Добавлено уточнение для корректного ввода значений

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Уnter the values separated by a space: ");
+string Values = Convert.ToString(Console.ReadLine());
+
+string[] X = Values.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+int[] array = X.Select(Int32.Parse).ToArray(); // Преобразование массива строк, в массив числовых значений
+int quantity = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+    if (array[i] > 0)
+    {
+        quantity++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(quantity);
